c0256f9f60ff81b27b1bf85aeefef889dfbe879b
analiabelo98 <analiabelo.42@gmail.com>
2021-04-21 14:01:39 +0000

    innerHTML

--- src/app.js
+++ src/app.js
@@ -12,11 +12,13 @@ window.onload = function() {
   let pronoun = ["the", "our"];
   let adj = ["great", "big"];
   let noun = ["jogger", "racoon"];
+  let text = document.getElementById("TheText");
 
   for (let i = 0; i < pronoun.length; i++) {
     for (let j = 0; j < adj.length; j++) {
       for (let k = 0; k < noun.length; k++) {
-        console.log(pronoun[i] + adj[j] + noun[k] + ".com");
+        text.innerHTML += `<p> ` + pronoun[i] + adj[j] + noun[k] + `.com <p>`;
+        //console.log(pronoun[i] + adj[j] + noun[k] + ".com");
       }
     }
   }
